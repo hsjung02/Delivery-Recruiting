a912df397790a2ba1aeaebf2f13c39a594fad104
Donghun Shin <sdh728@postech.ac.kr>
2021-08-03 17:51:10 +0900

    배달팟 모집 종료 화면을 만들었습니다.

--- new/src/Pages/EndOrder/EndOrder.js
+++ new/src/Pages/EndOrder/EndOrder.js
@@ -1,20 +1,59 @@
 import {Component} from "react";
+import axios from "axios";
+import {Message} from "semantic-ui-react";
 
 
 class EndOrder extends Component{
     constructor(props) {
         super(props);
+        this.ordername = this.props.match.name
+        this.endUrl="http://localhost:3001/order/end/"+this.ordername
+        this.state= {
+            totalPrice: "",
+            partyData: []
+        }
+    }
 
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    getData=async()=>{
+        const res = await axios.get(this.endUrl)
+        this.setState({
+            totalPrice: res.totalprice,
+            partyData: res.orders
+        })
+    }
+
+    endRecruit=async()=> {
+        await axios.post(this.endUrl)
+        window.location.href = "/"
     }
 
     render(){
 
         return(
             <div>
-                fjtjrn
+                최소 주문 금액: {this.state.totalPrice}<br/>
+                {this.state.partyData.map(item=>{
+                return(
+                <Message>
+                    주문자 전화번호 : {item.tel}<br/>
+                    주문한 음식 : {item.product}<br/>
+                    주문한 가격 : {item.price}<br/>
+                </Message>
+                )
+            })}
+                <Button onClick={this.endRecruit()}>
+                    모집 마감
+                </Button>
             </div>
+
         )
     }
+
 
 }
 
